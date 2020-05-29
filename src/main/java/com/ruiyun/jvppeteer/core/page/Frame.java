ce64bddc06d083704c47aa5b7359f4bdcdfcc1d2
fanyong920 <1023079644@qq.com>
2020-05-29 14:16:40 +0800

    2020-05-29删除System.out.println

--- src/main/java/com/ruiyun/jvppeteer/core/page/Frame.java
+++ src/main/java/com/ruiyun/jvppeteer/core/page/Frame.java
@@ -56,7 +56,6 @@ public class Frame {
         this.lifecycleEvents = new HashSet<>();
         this.mainWorld = new DOMWorld(frameManager, this, frameManager.getTimeoutSettings());
         this.secondaryWorld = new DOMWorld(frameManager, this, frameManager.getTimeoutSettings());
-        System.out.println("frameId:"+id+",domword:"+mainWorld+",secondaryWorld"+secondaryWorld);
         this.childFrames = new HashSet<>();
         if (this.parentFrame != null)
             this.parentFrame.getChildFrames().add(this);
