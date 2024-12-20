025eed67768dee5ff328a9697f804bd28c6b69a3
fanyong920 <1023079644@qq.com>
2024-12-20 10:11:44 +0800

    fix:TaskManager 中 task 属性改用 CopyOnWriteArraySet

--- src/main/java/com/ruiyun/jvppeteer/common/TaskManager.java
+++ src/main/java/com/ruiyun/jvppeteer/common/TaskManager.java
@@ -2,14 +2,14 @@ package com.ruiyun.jvppeteer.common;
 
 import com.ruiyun.jvppeteer.cdp.core.WaitTask;
 import com.ruiyun.jvppeteer.exception.JvppeteerException;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TaskManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(TaskManager.class);
-    private final Set<WaitTask> tasks = new HashSet<>();
+    private final Set<WaitTask> tasks = new CopyOnWriteArraySet<>();
 
     public void add(WaitTask task) {
         tasks.add(task);
