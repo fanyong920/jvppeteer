e00d414fbaf93e46e76899ea8a7c868f1af96127
fanyong920 <1023079644@qq.com>
2025-01-22 09:53:14 +0800

    update: PaperFormats添加以厘米为计算单位的注释

--- src/main/java/com/ruiyun/jvppeteer/cdp/entities/PaperFormats.java
+++ src/main/java/com/ruiyun/jvppeteer/cdp/entities/PaperFormats.java
@@ -1,17 +1,27 @@
 package com.ruiyun.jvppeteer.cdp.entities;
 
 public enum PaperFormats {
-
+    //21.59cm x 27.94cm
     letter(8.5, 11),
+    //21.59cm x 35.56cm
     legal(8.5, 14),
+    // 27.94cm x 43.18cm
     tabloid(11, 17),
+    // 43.18cm x 27.94cm
     ledger(17, 11),
+    // 84.1cm x 118.9cm
     a0(33.1, 46.8),
+    //59.4cm x 84.1cm
     a1(23.4, 33.1),
+    //42cm x 59.4cm
     a2(16.54, 23.4),
+    // 29.7cm x 42cm
     a3(11.7, 16.54),
+    //21cm x 29.7cm
     a4(8.27, 11.7),
+    //14.8cm x 21cm
     a5(5.83, 8.27),
+    //10.5cm x 14.8cm
     a6(4.13, 5.83);
 
     private double width;
