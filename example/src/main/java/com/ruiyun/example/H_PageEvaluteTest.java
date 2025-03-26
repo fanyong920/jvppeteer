9467d00c249b148ff6331384ecb55c297fab8939
fanyong920 <1023079644@qq.com>
2025-03-26 17:03:57 +0800

    test: 增加page.evaluate()的超时测试

--- example/src/main/java/com/ruiyun/example/H_PageEvaluteTest.java
+++ example/src/main/java/com/ruiyun/example/H_PageEvaluteTest.java
@@ -52,4 +52,45 @@ public class H_PageEvaluteTest extends A_LaunchTest {
             Thread.sleep(5000);
         }
     }
+
+    /**
+     * 超时设置
+     * @throws Exception 异常
+     */
+    @Test
+    public void test4() throws Exception {
+        //启动浏览器
+        try (Browser browser = Puppeteer.launch(launchOptions)) {
+            //打开一个页面
+            Page page = browser.newPage();
+            //监听devtool控制台输出
+            page.on(PageEvents.Console, (Consumer<ConsoleMessage>) consoleMessage -> System.out.println("console=" + consoleMessage.text()));
+            //在devtool 控制台打印输出
+            page.evaluate("function timeoutPromise(ms) {\n" +
+                    "  return new Promise((resolve, reject) => {\n" +
+                    "    const timeoutId = setTimeout(() => {\n" +
+                    "      reject(new Error('Promise timed out'));\n" +
+                    "    }, ms);\n" +
+                    "    // 示例使用\n" +
+                    "    const longRunningPromise = new Promise((resolve, reject) => {\n" +
+                    "      setTimeout(() => {\n" +
+                    "        resolve('Operation completed');\n" +
+                    "      }, 5000); // 模拟一个耗时5秒的操作\n" +
+                    "    });\n" +
+                    "    longRunningPromise.then(\n" +
+                    "      (res) => {\n" +
+                    "        clearTimeout(timeoutId);\n" +
+                    "        resolve(res);\n" +
+                    "      },\n" +
+                    "      (err) => {\n" +
+                    "        clearTimeout(timeoutId);\n" +
+                    "        reject(err);\n" +
+                    "      }\n" +
+                    "    );\n" +
+                    "  });\n" +
+                    "}", 2000);
+            //等待5s看看效果
+            Thread.sleep(5000);
+        }
+    }
 }
