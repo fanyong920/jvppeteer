be679af8efeffe3a4d52781123ba6dc5ad4a423e
fanyong920 <1023079644@qq.com>
2025-04-09 11:15:34 +0800

    fix: roll to Firefox 137.0 and Chrome 135.0.7049.42

--- src/main/java/com/ruiyun/jvppeteer/common/BrowserRevision.java
+++ src/main/java/com/ruiyun/jvppeteer/common/BrowserRevision.java
@@ -2,8 +2,8 @@ package com.ruiyun.jvppeteer.common;
 
 public class BrowserRevision {
 
-    private final static String CHROME_VERSION = "133.0.6943.98";
-    private final static String FIREFOX_VERSION = "stable_135.0";
+    private final static String CHROME_VERSION = "135.0.7049.42";
+    private final static String FIREFOX_VERSION = "stable_137.0";
 
     /**
      * 获取默认浏览器版本，最好使用默认指定的版本，否则有些cdp api参数会失效
