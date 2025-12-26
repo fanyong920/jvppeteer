ebdaa70db6238e9ffc5fd1e25db2d20fa6e24b5d
fanyong920 <1023079644@qq.com>
2025-12-26 21:25:06 +0800

    fix：修改Page.addStyleTag()的测试案例

--- example/src/main/java/com/ruiyun/example/P_AddStyleTagTest.java
+++ example/src/main/java/com/ruiyun/example/P_AddStyleTagTest.java
@@ -17,17 +17,13 @@ public class P_AddStyleTagTest {
         Browser browser = Puppeteer.launch(LAUNCHOPTIONS);
         //打开一个页面
         Page page = browser.newPage();
-        page.goTo("https://www.baidu.com/?tn=68018901_16_pg");
         FrameAddStyleTagOptions options = new FrameAddStyleTagOptions();
-        //修改一下百度一下按钮的颜色
-        options.setContent("#head_wrapper .s_btn{cursor:pointer;width:108px;height:44px;line-height:45px;line-height:44px\\9;padding:0;background:0 0;background-color:#b75014;border-radius:0 10px 10px 0;font-size:17px;color:#fff;box-shadow:none;font-weight:400;border:none;outline:0}");
+        options.setPath("E:\\puppeteer\\test\\assets\\injectedstyle.css");
         page.addStyleTag(options);
-        ScreenshotOptions screenshotOptions = new ScreenshotOptions();
-        screenshotOptions.setPath("baidu.png");
-        screenshotOptions.setFullPage(true);
-        page.screenshot(screenshotOptions);
-        page.$("#su").screenshot("baidu2.png");
-        Thread.sleep(9000);
+        Object evaluate = page.evaluate(
+                "window.getComputedStyle(document.querySelector('body')).getPropertyValue('background-color')"
+        );
+        System.out.println("evaluate=" + evaluate);
         browser.close();
     }
 
